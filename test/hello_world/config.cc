61c9f7f1ade767f66db201a068e50abdd980ddbb
Rick Appleton <ricka@daedalus-development.net>
2020-02-08 11:16:29 +0100

    Renamed project description files to .cc

---
+++ test/hello_world/config.cc
@@ -0,0 +1,25 @@
+#include "../../source/cconstruct.h"
+
+int main()
+{
+  auto cc = cc_xcode_builder;
+  cc.workspace.setOutputFolder("test");
+
+  cc.workspace.addPlatform("Win32", EPlatformTypeX86);
+  cc.workspace.addPlatform("x64", EPlatformTypeX64);
+  cc.workspace.addConfiguration("Debug");
+  cc.workspace.addConfiguration("Release");
+
+  auto p = cc.project.create("hello_world", CCProjectTypeConsoleApplication);
+  const char *srcFiles[] = {"src/main.c", "src/function.c", NULL};
+  for (auto f : srcFiles)
+  {
+    if (f)
+      cc.project.addFile(p, f, "Source Files");
+  }
+  cc.workspace.addProject(p);
+
+  cc.generate();
+
+  return 0;
+}
---
+++ test/library_dependency/config.cc
@@ -0,0 +1,27 @@
+#include "../../source/cconstruct.h"
+
+int main()
+{
+  auto cc = cc_xcode_builder;
+  cc.workspace.setOutputFolder("test");
+  cc.workspace.setLabel("library_dependency");
+
+  cc.workspace.addPlatform("Win32", EPlatformTypeX86);
+  cc.workspace.addPlatform("x64", EPlatformTypeX64);
+  cc.workspace.addConfiguration("Debug");
+  cc.workspace.addConfiguration("Release");
+
+  auto l = cc.project.create("my_library", CCProjectTypeStaticLibrary);
+  cc.project.addFile(l, "src/library.c", "Source Files");
+
+  auto b = cc.project.create("my_binary", CCProjectTypeConsoleApplication);
+  cc.project.addFile(b, "src/main.c", "Source Files");
+  cc.project.addInputProject(b, l);
+
+  cc.workspace.addProject(l);
+  cc.workspace.addProject(b);
+
+  cc.generate();
+
+  return 0;
+}
