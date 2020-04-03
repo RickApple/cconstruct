f37292930b4c9b2928e6ff19cc39787577d866ab
Rick Appleton <ricka@daedalus-development.net>
2020-04-03 15:19:59 +0200

    Reordered test cases

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -2,46 +2,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 echo on
 
-pushd compile_flags
-
-rd /S /Q build
-
-cl.exe -EHsc -Fe: cconstruct.exe config.cc
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cconstruct.exe
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-devenv.exe build\workspace.sln /Build "Release|x64"
-if %errorlevel% neq 1 exit /b %errorlevel%
-REM building should cause an error because flag has been added to set warnings as errors
-
-popd
-
-pushd preprocessor
-
-rd /S /Q build
-
-cl.exe -EHsc -Fe: cconstruct.exe config.cc
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cconstruct.exe
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-devenv.exe build\workspace.sln /Build "Debug|x64"
-if %errorlevel% neq 0 exit /b %errorlevel%
-build\x64\Debug\preprocessor.exe
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-devenv.exe build\workspace.sln /Build "Release|x64"
-if %errorlevel% neq 0 exit /b %errorlevel%
-build\x64\Release\preprocessor.exe
-rem the release build is expected to give an error, since the preprocessor has a different value for that build
-if %errorlevel% neq 1 exit /b %errorlevel%
-
-popd
-
-
 
 pushd hello_world
 
@@ -101,6 +61,49 @@ popd
 
 
 
+pushd preprocessor
+
+rd /S /Q build
+
+cl.exe -EHsc -Fe: cconstruct.exe config.cc
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cconstruct.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+devenv.exe build\workspace.sln /Build "Debug|x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+build\x64\Debug\preprocessor.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+devenv.exe build\workspace.sln /Build "Release|x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+build\x64\Release\preprocessor.exe
+rem the release build is expected to give an error, since the preprocessor has a different value for that build
+if %errorlevel% neq 1 exit /b %errorlevel%
+
+popd
+
+
+
+pushd compile_flags
+
+rd /S /Q build
+
+cl.exe -EHsc -Fe: cconstruct.exe config.cc
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cconstruct.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+devenv.exe build\workspace.sln /Build "Release|x64"
+if %errorlevel% neq 1 exit /b %errorlevel%
+REM building should cause an error because flag has been added to set warnings as errors
+
+popd
+
+
+
 
 
 pushd ..\tools
@@ -115,6 +118,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 devenv.exe build\workspace.sln /Build Debug
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+rem Create output folder for combined file
+mkdir ..\build
 build\Debug\cconstruct_release.exe ../source/cconstruct.h ../build/cconstruct_release.h
 if %errorlevel% neq 0 exit /b %errorlevel%
 
