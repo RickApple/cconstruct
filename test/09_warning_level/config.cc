9bb0f0860180ddf67aa599b1bb259eef1bc414c0
Rick Appleton <ricka@daedalus-development.net>
2020-04-15 10:26:26 +0200

    Added testcase for warning settings.

---
+++ test/09_warning_level/config.cc
@@ -0,0 +1,33 @@
+#include "../../source/cconstruct.h"
+
+int main() {
+  CCPlatformHandle platform_x64 = cc.createPlatform(EPlatformTypeX64);
+  cc.workspace.addPlatform(platform_x64);
+
+  CCConfigurationHandle configuration_debug   = cc.createConfiguration("Debug");
+  CCConfigurationHandle configuration_release = cc.createConfiguration("Release");
+  cc.workspace.addConfiguration(configuration_debug);
+  cc.workspace.addConfiguration(configuration_release);
+
+  void* p             = cc.createProject("warning_level", CCProjectTypeConsoleApplication, NULL);
+  const char* files[] = {"src/main.c"};
+
+  cc.project.addFiles(p, countof(files), files, NULL);
+
+  cc_flags flags;
+  cc.state.reset(&flags);
+  // Debug build give warnings which are by default turned into errors.
+  // Disabling warnings as errors still shows the warnings during compile, but no errors.
+  cc.state.disableWarningsAsErrors(&flags);
+  cc.project.setFlagsLimited(p, &flags, platform_x64, configuration_debug);
+  cc.state.reset(&flags);
+  // Another way to prevent these warnings is to turn down the warning level, in this case all the
+  // way off. NOT RECOMMENDED!
+  cc.state.setWarningLevel(&flags, EStateWarningLevelNone);
+  cc.project.setFlagsLimited(p, &flags, platform_x64, configuration_release);
+
+  // TODO: show case the different warning levels with different settings for different files
+  cc_default_generator("build");
+
+  return 0;
+}
