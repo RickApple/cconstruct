b5b2c9f1b3136b6034ca56818f5411c5e6079616
Rick Appleton <ricka@daedalus-development.com>
2025-04-30 10:00:23 +0200

    xcode choose arch

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -13,7 +13,7 @@ pushd 01_hello_world
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme hello_world
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme hello_world -destination 'platform=macOS,arch=x86_64'
 #-sdk "*targetSDK*" -configuration *buildConfig* CODE_SIGN_IDENTITY="*NameOfCertificateIdentity*" PROVISIONING_PROFILE="*ProvisioningProfileName" OTHER_CODE_SIGN_FLAGS="--keychain *keyChainName*"
 ./build/x64/Debug/hello_world
 popd
@@ -23,7 +23,7 @@ pushd 02_include_folders
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme include_folders
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme include_folders -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/include_folders
 popd
 
@@ -32,7 +32,7 @@ pushd 03_library_dependency
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/xcode/library_dependency.xcworkspace -scheme my_binary
+xcodebuild -quiet -workspace build/xcode/library_dependency.xcworkspace -scheme my_binary -destination 'platform=macOS,arch=x86_64'
 ./build/xcode/x64/Debug/my_binary
 popd
 
@@ -41,10 +41,10 @@ pushd 03a_library_dependency_explicit
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct  --generate-projects
-xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_library
-xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_binary
-xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_library -configuration Release
-xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_binary -configuration Release
+xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_library -destination 'platform=macOS,arch=x86_64'
+xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_binary -destination 'platform=macOS,arch=x86_64'
+xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_library -configuration Release -destination 'platform=macOS,arch=x86_64'
+xcodebuild -quiet -workspace build/xcode/library_dependency_explicit.xcworkspace -scheme my_binary -configuration Release -destination 'platform=macOS,arch=x86_64'
 set +e
 # Debug lib returns 1
 build/xcode/x64/Debug/bin/my_binary
@@ -61,13 +61,13 @@ pushd 04_preprocessor
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme preprocessor -configuration Debug
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme preprocessor -configuration Debug -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/preprocessor
 if [ $? -ne 0 ]
 then
   exit 1
 fi
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme preprocessor -configuration Release
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme preprocessor -configuration Release -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Release/preprocessor   
 if [ $? -eq 0 ]
 then
@@ -82,7 +82,7 @@ pushd 05_compile_flags
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme compile_flags
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme compile_flags -destination 'platform=macOS,arch=x86_64'
 # building should cause an error because flag has been added to set warnings as errors
 if [ $? -eq 0 ]
 then
@@ -97,7 +97,7 @@ rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
 set +e
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme post_build_action
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme post_build_action -destination 'platform=macOS,arch=x86_64'
 # building should fail due to the unknown post build command
 if [ $? -eq 0 ]
 then
@@ -115,7 +115,7 @@ rm -rf build
 cp return_value1.inl return_value.inl
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme changed_config
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme changed_config -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/changed_config
 # The first config builds the program so that it returns 1, so check for that specifically
 if [ $? -ne 1 ]
@@ -124,7 +124,7 @@ then
 fi
 cp return_value2.inl return_value.inl
 ./cconstruct
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme changed_config
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme changed_config -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/changed_config
 # The second config builds the program so that it returns 2, so check for that specifically
 if [ $? -ne 2 ]
@@ -139,7 +139,7 @@ pushd 08_project_structure
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/project_structure.xcworkspace -scheme my_binary
+xcodebuild -quiet -workspace build/project_structure.xcworkspace -scheme my_binary -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/my_binary
 popd
 
@@ -149,8 +149,8 @@ rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
 # Without doing something about warnings, the following builds would fail.
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme warning_level -configuration Debug
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme warning_level -configuration Release
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme warning_level -configuration Debug -destination 'platform=macOS,arch=x86_64'
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme warning_level -configuration Release -destination 'platform=macOS,arch=x86_64'
 popd
 
 
@@ -158,7 +158,7 @@ pushd 10_mixing_c_and_cpp
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme mixing_c_and_cpp
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme mixing_c_and_cpp -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/mixing_c_and_cpp
 popd
 
@@ -167,7 +167,7 @@ pushd 11_nested_folders
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/src/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme nested_folders
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme nested_folders -destination 'platform=macOS,arch=x86_64'
 ./build/x64/Debug/nested_folders
 popd
 
@@ -179,13 +179,13 @@ mkdir build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/project/config.cc -o build/cconstruct
 pushd build
 ./cconstruct
-xcodebuild -quiet -workspace workspace.xcworkspace -scheme config_folders
+xcodebuild -quiet -workspace workspace.xcworkspace -scheme config_folders -destination 'platform=macOS,arch=x86_64'
 ./x64/Debug/config_folders
 popd
 # Also check if it works when calling it from the parent folder
 rm -rf build/*.xc*
 ./build/cconstruct
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme config_folders
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme config_folders -destination 'platform=macOS,arch=x86_64'
 popd
 
 
@@ -193,7 +193,7 @@ pushd 13_cpp_config
 rm -rf build
 $COMPILE_CCONSTRUCT_CPP_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme cpp_config
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme cpp_config -destination 'platform=macOS,arch=x86_64'
 popd
 
 
@@ -201,7 +201,7 @@ pushd 14_c_config
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme c_config
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme c_config -destination 'platform=macOS,arch=x86_64'
 popd
 
 
@@ -209,7 +209,7 @@ pushd 15_other_file_types
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme other_file_types
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme other_file_types -destination 'platform=macOS,arch=x86_64'
 popd
 
 
@@ -219,7 +219,7 @@ $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
 TEST_TIME=`date`
 echo $TEST_TIME > src/test_source.txt
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme custom_commands
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme custom_commands -destination 'platform=macOS,arch=x86_64'
 CMD_OUTPUT=$(./build/x64/Debug/custom_commands)
 [ "$CMD_OUTPUT" != "test $TEST_TIME" ] && exit 1
 popd
@@ -229,7 +229,7 @@ pushd 19_macos_framework
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct --generate-projects
-xcodebuild -quiet -workspace build/xcode/macos_framework.xcworkspace -scheme my_binary
+xcodebuild -quiet -workspace build/xcode/macos_framework.xcworkspace -scheme my_binary -destination 'platform=macOS,arch=x86_64'
 ./build/xcode/x64/Debug/my_binary
 popd
 
@@ -263,7 +263,7 @@ pushd ../tools
 rm -rf build
 $COMPILE_CCONSTRUCT_COMMAND $PWD/config.cc -o cconstruct
 ./cconstruct
-xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme cconstruct_release
+xcodebuild -quiet -workspace build/workspace.xcworkspace -scheme cconstruct_release -destination 'platform=macOS,arch=x86_64'
 mkdir -p ../build
 ./build/x64/Debug/cconstruct_release ../source/cconstruct.h ../build/cconstruct.h
 popd
