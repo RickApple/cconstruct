45e6dc8daa687fdf8525654ac45e4be3d074f787
Rick Appleton <ricka@daedalus-development.com>
2025-05-01 10:11:09 +0200

    refactor: change test to use msbuild

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -3,6 +3,8 @@ echo on
 @set COMPILE_CONSTRUCT_COMMAND=cl.exe /W4 /WX /FC /Fo%TEMP% /Fecconstruct.exe /nologo /TC
 @set COMPILE_CONSTRUCT_CPP_COMMAND=cl.exe /W4 /WX /FC /Fo%TEMP% /Fecconstruct.exe /nologo /TP
 
+@set BUILD_DEBUG_COMMAND=msbuild /p:Configuration=Debug /p:Platform=x64
+@set BUILD_RELEASE_COMMAND=msbuild /p:Configuration=Release /p:Platform=x64
 
 echo %username%
 
@@ -18,7 +20,7 @@ pushd 22_cconstruct_architecture
 call "%VSPATH%\VC\Auxiliary\Build\vcvars64.bat"
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\amd64\cconstruct_architecture.exe
 if %errorlevel% neq 64 exit /b %errorlevel%
 @popd
@@ -29,7 +31,7 @@ if %errorlevel% neq 64 exit /b %errorlevel%
 call "%VSPATH%\VC\Auxiliary\Build\vcvars32.bat"
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x86\cconstruct_architecture.exe
 if %errorlevel% neq 86 exit /b %errorlevel%
 popd
@@ -49,7 +51,7 @@ pushd 01_hello_world
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\hello_world.exe || exit /b
 popd
 
@@ -58,7 +60,7 @@ pushd 02_include_folders
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\include_folders.exe || exit /b
 popd
 
@@ -67,7 +69,7 @@ pushd 03_library_dependency
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\msvc\library_dependency.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\msvc\library_dependency.sln || exit /b
 build\msvc\x64\Debug\my_binary.exe || exit /b
 popd
 
@@ -76,14 +78,14 @@ pushd 03a_library_dependency_explicit
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_library /Build "Debug|x64" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_binary /Build "Debug|x64" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_library /Build "Debug|x86" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_binary /Build "Debug|x86" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_library /Build "Release|x64" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_binary /Build "Release|x64" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_library /Build "Release|x86" || exit /b
-devenv.com build\msvc\library_dependency_explicit.sln /Project my_binary /Build "Release|x86" || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_library /p:Configuration=Debug;Platform=x64 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_binary /p:Configuration=Debug;Platform=x64 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_library /p:Configuration=Debug;Platform=x86 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_binary /p:Configuration=Debug;Platform=x86 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_library /p:Configuration=Release;Platform=x64 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_binary /p:Configuration=Release;Platform=x64 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_library /p:Configuration=Release;Platform=x86 || exit /b
+msbuild build\msvc\library_dependency_explicit.sln /t:my_binary /p:Configuration=Release;Platform=x86 || exit /b
 rem Debug lib returns 1
 build\msvc\x64\Debug\bin\my_binary.exe
 if %errorlevel% neq 1 exit /b %errorlevel%
@@ -101,14 +103,14 @@ pushd 04_preprocessor
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\preprocessor.exe
 rem Debug build has the expected value for the define, so should return 0
-if %errorlevel% neq 0 exit /b %errorlevel%
-devenv.com build\workspace.sln /Build "Release|x64" || exit /b
+if %errorlevel% neq 0 exit /b 1
+%BUILD_RELEASE_COMMAND% build\workspace.sln || exit /b
 build\x64\Release\preprocessor.exe
 rem Release build is expected to return 1, since the define has a different value for that build
-if %errorlevel% neq 1 exit /b %errorlevel%
+if %errorlevel% neq 1 exit /b 1
 popd
 
 
@@ -116,7 +118,7 @@ pushd 05_compile_flags
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Release|x64"
+%BUILD_RELEASE_COMMAND% build\workspace.sln
 if %errorlevel% neq 1 exit /b %errorlevel%
 REM building should cause an error because flag has been added to set warnings as errors
 popd
@@ -126,7 +128,7 @@ pushd 06_post_build_action
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64"
+%BUILD_DEBUG_COMMAND% build\workspace.sln
 rem The build is expected to give an error, since the post build action doesn't succeed ...
 if %errorlevel% equ 0 exit /b %errorlevel%
 rem ... However, the executable has been built, so test it is there and works correctly
@@ -139,20 +141,20 @@ rd /S /Q build
 copy return_value1.inl return_value.inl
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\changed_config.exe
 rem The first config builds the program so that it returns 1, so check for that specifically
 if %errorlevel% neq 1 exit /b %errorlevel%
 copy return_value2.inl return_value.inl
 cconstruct.exe
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\changed_config.exe
 rem The second config builds the program so that it returns 2, so check for that specifically
 if %errorlevel% neq 2 exit /b %errorlevel%
 rem Now set back the first config, but don't rebuild
 copy return_value1.inl return_value.inl
 cconstruct.exe --generate-projects
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\changed_config.exe
 if %errorlevel% neq 2 exit /b %errorlevel%
 popd
@@ -162,7 +164,7 @@ pushd 08_project_structure
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\project_structure.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\project_structure.sln || exit /b
 build\x64\Debug\my_binary.exe || exit /b
 popd
 
@@ -172,8 +174,8 @@ rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
 rem Without doing something about warnings, the following builds would fail.
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
-devenv.com build\workspace.sln /Build "Release|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
+%BUILD_RELEASE_COMMAND% build\workspace.sln || exit /b
 popd
 
 
@@ -181,7 +183,7 @@ pushd 10_mixing_c_and_cpp
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\mixing_c_and_cpp.exe || exit /b
 popd
 
@@ -190,7 +192,7 @@ pushd 11_nested_folders
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% src/config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\nested_folders.exe || exit /b
 popd
 
@@ -202,13 +204,13 @@ mkdir build
 cl.exe /EHsc /Fo%TEMP% /FC /Febuild/cconstruct.exe /nologo /TC project/config.cc || exit /b
 pushd build
 cconstruct.exe --generate-projects || exit /b
-devenv.com workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% workspace.sln || exit /b
 x64\Debug\config_folders.exe || exit /b
 popd
 REM also check if it works when calling it from a different folder
 del build\config_folder.vcxproj.*
 build\cconstruct.exe || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 popd
 
 
@@ -216,7 +218,7 @@ pushd 13_cpp_config
 rd /S /Q build
 %COMPILE_CONSTRUCT_CPP_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 popd
 
 
@@ -224,7 +226,7 @@ pushd 14_c_config
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 popd
 
 
@@ -232,7 +234,7 @@ pushd 15_other_file_types
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 popd
 
 
@@ -240,7 +242,7 @@ pushd 16_windowed_application
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\windowed_application.exe || exit /b
 popd
 
@@ -249,7 +251,7 @@ pushd 17_link_flags
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe --generate-projects || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 if not exist build\x64\Debug\link_flags_named.pdb (
   exit 1
 )
@@ -263,7 +265,7 @@ del src\test.txt
 cconstruct.exe --generate-projects || exit /b
 set TEST_TIME=%time%
 echo %TEST_TIME%>src\test_source.txt
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 rem copy src\test_source.txt src\test.txt
 rem Get output into variable
 FOR /F "tokens=* USEBACKQ" %%F IN (`build\x64\Debug\custom_commands.exe`) DO (
@@ -289,7 +291,7 @@ if %errorlevel% neq 2 exit /b %errorlevel%
 rem No error at all, everything goes OK from here on out
 copy error_none.inl error.inl
 cconstruct.exe || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 build\x64\Debug\errors.exe || exit /b
 popd
 
@@ -301,7 +303,7 @@ pushd ..\tools
 rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 cconstruct.exe || exit /b
-devenv.com build\workspace.sln /Build "Debug|x64" || exit /b
+%BUILD_DEBUG_COMMAND% build\workspace.sln || exit /b
 rem Create output folder for combined file
 mkdir ..\build
 build\x64\Debug\cconstruct_release.exe ../source/cconstruct.h ../build/cconstruct.h || exit /b
