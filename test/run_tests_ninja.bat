7c6323b5f0141fd0a45c594c925cc661c11e0064
Rick Appleton <ricka@daedalus-development.com>
2025-05-07 15:57:38 +0200

    refactor: make cconstruct command a variable

--- test/run_tests_ninja.bat
+++ test/run_tests_ninja.bat
@@ -2,6 +2,7 @@ echo on
 @set COMPILE_DEBUG_CONSTRUCT_COMMAND=cl.exe /ZI /W4 /WX /DEBUG /FC /Fo%TEMP% /Fecconstruct.exe /nologo /TC
 @set COMPILE_CONSTRUCT_COMMAND=cl.exe /W4 /WX /FC /Fo%TEMP% /Fecconstruct.exe /nologo /TC
 @set COMPILE_CONSTRUCT_CPP_COMMAND=cl.exe /W4 /WX /FC /Fo%TEMP% /Fecconstruct.exe /nologo /TP
+@set CMD_CONSTRUCT_WORKSPACE=cconstruct.exe --generator=ninja --generate-projects
 @set BUILD_COMMAND=ninja -C build
 
 @rem Find location of Visual Studio
@@ -21,7 +22,7 @@ rem call "%VSPATH%\VC\Auxiliary\Build\vcvars64.bat"
 pushd 01_hello_world
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe  --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
 build\x64\Debug\hello_world.exe || exit /b
 popd
@@ -30,7 +31,7 @@ popd
 pushd 02_include_folders
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
 build\x64\Debug\include_folders.exe || exit /b
 popd
@@ -39,7 +40,7 @@ popd
 pushd 03_library_dependency
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
 build\x64\Debug\my_binary.exe || exit /b
 popd
@@ -48,12 +49,12 @@ popd
 pushd 04_preprocessor
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
 build\x64\Debug\preprocessor.exe
 rem Debug build has the expected value for the define, so should return 0
 if %errorlevel% neq 0 exit /b 1
-cconstruct.exe --generator=ninja --generate-projects --config=Release || exit /b
+%CMD_CONSTRUCT_WORKSPACE% --config=Release || exit /b
 %BUILD_COMMAND% --verbose || exit /b
 build\x64\Release\preprocessor.exe
 rem Release build is expected to return 1, since the define has a different value for that build
@@ -63,7 +64,7 @@ popd
 pushd 05_compile_flags
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND%
 if %errorlevel% neq 1 exit /b %errorlevel%
 REM building should cause an error because flag has been added to set warnings as errors
@@ -72,7 +73,7 @@ popd
 pushd 06_post_build_action
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND%
 rem The build is expected to give an error, since the post build action doesn't succeed ...
 if %errorlevel% equ 0 exit /b %errorlevel%
@@ -87,7 +88,7 @@ mkdir build
 copy return_value1.inl return_value.inl
 echo. >> return_value.inl
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects
+%CMD_CONSTRUCT_WORKSPACE%
 pushd build
 ninja || exit /b
 x64\Debug\changed_config.exe
@@ -113,9 +114,9 @@ pushd 09_warning_level
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
 rem Without doing something about warnings, the following builds would fail.
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
-cconstruct.exe --generator=ninja --generate-projects --config=Release|| exit /b
+%CMD_CONSTRUCT_WORKSPACE% --config=Release || exit /b
 %BUILD_COMMAND% || exit /b
 popd
 
@@ -123,7 +124,7 @@ popd
 pushd 10_mixing_c_and_cpp
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND%  || exit /b
 build\x64\Debug\mixing_c_and_cpp.exe || exit /b
 popd
@@ -132,7 +133,7 @@ popd
 pushd 11_nested_folders
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% src/config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
 build\x64\Debug\nested_folders.exe || exit /b
 popd
@@ -144,7 +145,7 @@ REM this test requires the build folder be there
 mkdir build 
 cl.exe /EHsc /Fo%TEMP% /FC /Febuild/cconstruct.exe /nologo /TC /ZI /DEBUG project/config.cc || exit /b
 pushd build
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 ninja || exit /b
 x64\Debug\config_folders.exe || exit /b
 popd
@@ -158,7 +159,7 @@ popd
 pushd 13_cpp_config
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_CPP_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND% || exit /b
 popd
 
@@ -166,7 +167,7 @@ popd
 pushd 17_link_flags
 if exist build rd /S /Q build
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 %BUILD_COMMAND%  || exit /b
 if not exist build\x64\Debug\link_flags_named.pdb (
   exit 1
@@ -178,7 +179,7 @@ pushd 18_custom_commands
 if exist build rd /S /Q build
 del src\test.txt
 %COMPILE_CONSTRUCT_COMMAND% config.cc || exit /b
-cconstruct.exe --generator=ninja --generate-projects || exit /b
+%CMD_CONSTRUCT_WORKSPACE% || exit /b
 set TEST_TIME=%time%
 echo %TEST_TIME%>src\test_source.txt
 %BUILD_COMMAND% || exit /b
